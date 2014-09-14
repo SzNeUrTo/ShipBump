1a70e3fc42fdf83a1c9cf50eff3eaec355801405
ZeroMAN <Supanut.Overload@Gmail.com>
2014-09-15 01:08:17 +0700

    Show Mouse Position

--- src/shipbump/ShipBumpGame.java
+++ src/shipbump/ShipBumpGame.java
@@ -4,13 +4,17 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.lwjgl.input.Mouse;
 
 
 public class ShipBumpGame extends BasicGame{
 
+	private String mouse_position = "";
 	private static final int GAME_WIDTH = 640;
 	private static final int GAME_HEIGHT = 480;
+	private Ship ship;
 
 	public ShipBumpGame(String title) {
 		super(title);
@@ -18,22 +22,23 @@ public class ShipBumpGame extends BasicGame{
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-
-		
+		g.drawString(mouse_position, GAME_WIDTH*3/5, 10);
+		ship.draw();	
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-
+		ship = new Ship(GAME_WIDTH/2, GAME_HEIGHT/2);
 		
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
+		ship.movement();
+		mouse_position = "Mouse Position X : " + Mouse.getX() + "\nMouse Position Y : " + Mouse.getY();
 
-		
 	}
-	
+
 	public static void main(String[] args) {
 		try {
 			ShipBumpGame game = new ShipBumpGame("Ship Bump Game");
