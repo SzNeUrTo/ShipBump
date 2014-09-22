b32ccd1f31b01e3fc842dcd192696eb15506c01e
ZeroMAN <Supanut.Overload@Gmail.com>
2014-09-22 14:52:24 +0700

    Rotation Ship

--- src/shipbump/Ship.java
+++ src/shipbump/Ship.java
@@ -8,6 +8,8 @@ public class Ship {
 	private Image image;
 	private float x;
 	private float y;
+	private float newX;
+	private float newY;
 	
 	public Ship(float x, float y) throws SlickException {
 		image = new Image("res/ship.png");
@@ -28,8 +30,30 @@ public class Ship {
 	}
 
 	public void movement(float mouseX, float mouseY) {
-		this.x = mouseX - image.getWidth() / 2;
-		this.y = ShipBumpGame.GAME_HEIGHT - mouseY - image.getHeight() / 2;
+		newX = mouseX - image.getWidth() / 2;
+		newY = ShipBumpGame.GAME_HEIGHT - mouseY - image.getHeight() / 2;
+		
+		float angle;
+		
+		if (newX != this.x && newY != this.y) {
+			angle = (float) Math.acos((newX - this.x) / (Math.sqrt((newX - this.x) * (newX - this.x) + (newY - this.y) * (newY - this.y))));
+			if (newY > this.y) {
+				image.setRotation((float) (180 * angle / Math.PI));
+			}
+			else {
+				image.setRotation((float) (-180 * angle / Math.PI));
+			}
+			
+				
+			System.out.println();
+		}
+		else {
+			angle = 0;
+		}
+		this.x = newX;
+		this.y = newY;
+		
+		System.out.println("Old X : " + this.x + " New X : " + newX +" Old Y : " + this.y + " New Y : " + newY + " Angle : " + angle);
 	}
 
 
