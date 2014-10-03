206af68fa5e998f723dcc0917228c1901ddefa41
ZeroMAN <Supanut.Overload@Gmail.com>
2014-10-03 19:57:55 +0700

    create taaget dummy x y

--- src/shipbump/ExtraterrestrialMaterial.java
+++ src/shipbump/ExtraterrestrialMaterial.java
@@ -10,22 +10,31 @@ import org.newdawn.slick.SlickException;
 public class ExtraterrestrialMaterial implements Entity {
 	
 	private Image image;
-	float x;
-	float y;
 	private float angle=0;
+	private float x;
+	private float y;
+	private float targetDummyX;
+	private float targetDummyY;
+	private Random random = new Random();
+	
 	
 	public ExtraterrestrialMaterial() throws SlickException {
 		image = new Image("res/obtacle.png");
 		initPosition();
+		initTargetDummyXY();
 	}
 	
+	//for check collision dummyXY
+	private void initTargetDummyXY() {
+		this.targetDummyX = random.nextInt(ShipBumpGame.GAME_WIDTH * 4 / 5) + ShipBumpGame.GAME_WIDTH / 5;
+		this.targetDummyY = random.nextInt(ShipBumpGame.GAME_HEIGHT * 4 / 5) + ShipBumpGame.GAME_HEIGHT / 5;
+	}
+
 	private void initPosition() {
-		Random random = new Random();
 		selectQuadrant();
 	}
 	
 	private void selectQuadrant() {
-		Random random = new Random();
 		int quadrant = random.nextInt(4) + 1;
 		if (quadrant == 1) {
 			quadrant_1(); 
@@ -39,33 +48,28 @@ public class ExtraterrestrialMaterial implements Entity {
 	}
 	
 	private void quadrant_1() {
-		Random random = new Random();
 		this.x = ShipBumpGame.GAME_WIDTH + this.image.getWidth() * 2;
 		this.y = random.nextInt(ShipBumpGame.GAME_HEIGHT * 5 / 4) - ShipBumpGame.GAME_HEIGHT / 4;
 	}
 
 	private void quadrant_2() {
-		Random random = new Random();
 		this.x = random.nextInt(ShipBumpGame.GAME_WIDTH * 5 / 4) - ShipBumpGame.GAME_WIDTH / 4; 
 		this.y = ShipBumpGame.GAME_HEIGHT + this.image.getHeight() * 2;
 	}
 	
 	private void quadrant_3() {
-		Random random = new Random();
 		this.x = -this.image.getWidth() * 2; 
 		this.y = random.nextInt(ShipBumpGame.GAME_HEIGHT * 5 / 4) - ShipBumpGame.GAME_HEIGHT / 4;
 	}
 	
 	private void quadrant_4() {
-		Random random = new Random();
 		this.x = random.nextInt(ShipBumpGame.GAME_WIDTH * 5 / 4) - ShipBumpGame.GAME_WIDTH / 4; 
 		this.y = -this.image.getHeight() * 2;
 	}
 	
 	@Override
 	public void render(Graphics graphics) {
-		this.image.draw(this.x, this.y);
-		
+		this.image.draw(this.x, this.y);	
 	}
 	
 	@Override
