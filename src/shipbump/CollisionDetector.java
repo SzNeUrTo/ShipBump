22bf712e7aab2fa17eb487e73ce5823bb362c321
ZeroMAN <Supanut.Overload@Gmail.com>
2014-10-04 22:26:50 +0700

    Create Class CollisionDetector --> Extract EMColideBorder

---
+++ src/shipbump/CollisionDetector.java
@@ -0,0 +1,19 @@
+package shipbump;
+
+public class CollisionDetector {
+//
+//	public CollisionDetector() {
+//		
+//	}
+	public static boolean isEMCollideBullet(float emX, float emY, float bulletX, float bulletY) {
+		return false;
+	}
+	
+	public boolean isEMCollideBorder(float emX, float emY) {
+		return false;
+	}
+	
+	public boolean isEMCollideShip(float emX, float emY, float shipX, float shipY) {
+		return false;
+	}
+}
